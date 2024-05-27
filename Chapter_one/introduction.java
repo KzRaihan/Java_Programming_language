918abae0c43cab7ccfe6abaa94441f52ec19091f
KzRaihan <kamruzzamanraihan00@gmail.com>
2024-05-27 16:14:09 +0600

    start java program

---
+++ Chapter_one/introduction.java
@@ -0,0 +1,8 @@
+public class introduction {
+    public static void main(String[] args)
+    {
+        System.out.println("This is a simple Java program");
+        System.out.println("Name: Md Kamruzzaman");
+        System.out.println("ID: CSE2103024039");
+    }
+}
---
+++ Problme_Solution/Factorical_number.java
@@ -0,0 +1,22 @@
+/*
+ * 1. Write a program to find the factorial of a number.
+ */
+import java.util.*;
+public class Factorical_number {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter any integer: ");
+        int num = sc.nextInt();
+
+        int fact = 1;
+        for (int i = 1; i <= num; i++)
+        {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + num + " is " + fact);
+
+
+
+    }
+}
